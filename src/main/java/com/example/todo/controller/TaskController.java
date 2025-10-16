7cf9a95dfd3dc83b7705e9420d9da311d2bf92b8
Nikitha Yohannan <nikithayohannan28@gmail.com>
2025-10-16 13:44:29 +0530

    Initial commit - TodoBackend Spring Boot project

---
+++ src/main/java/com/example/todo/controller/TaskController.java
@@ -0,0 +1,45 @@
+package com.example.todo.controller;
+
+import com.example.todo.model.Task;
+import com.example.todo.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Integer id) {
+        return taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Integer id, @RequestBody Task taskDetails) {
+        Task task = taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setCompleted(taskDetails.getCompleted());
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Integer id) {
+        taskRepository.deleteById(id);
+    }
+}
